034d5ce41a8ae7c675ac7dd456e6dee9c51f3bfe
Trevor Sears <trevorsears.main@gmail.com>
2022-01-15 09:21:17 -0500

    Added a page router to the `index.tsx` app root.

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,24 @@ import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import { Home } from "./react/pages/home";
 import { AnnouncementBanner } from "./react/elements/announcement-banner";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { AboutUs } from "./react/pages/about-us";
+import { NotFound } from "./react/pages/not-found";
+import { ContactUs } from "./react/pages/contact-us";
 
 ReactDOM.render(
     <React.StrictMode>
-        <AnnouncementBanner>Due to the rising COVID-19 numbers in our target age group (2-12 year olds) we have decided to postpone Super Science Saturday. This was a tough decision to make, but we feel for the safety of all involved that this is the best course of action. We are hoping to reschedule this event for the Spring of 2022.</AnnouncementBanner>
-        <Home />
+        <BrowserRouter>
+            <AnnouncementBanner>Due to the rising COVID-19 numbers in our target age group (2-12 year olds) we have decided to postpone Super Science Saturday. This was a tough decision to make, but we feel for the safety of all involved that this is the best course of action. We are hoping to reschedule this event for the Spring of 2022.</AnnouncementBanner>
+            <Routes>
+                <Route index element={<Navigate replace to="/home" />} />
+                <Route path="/index" element={<Navigate replace to="/home" />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/about-us" element={<AboutUs />} />
+                <Route path="/contact-us" element={<ContactUs />} />
+                <Route path="*" element={<NotFound />} />
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('react-root')
 );
