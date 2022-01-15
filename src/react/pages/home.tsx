4e096e197c9441e54d77ff84b2f4586444f10ba6
Trevor Sears <trevorsears.main@gmail.com>
2022-01-15 09:16:44 -0500

    Slightly updated homepage copy.

--- src/react/pages/home.tsx
+++ src/react/pages/home.tsx
@@ -15,7 +15,14 @@ export class Home extends React.Component<Props, State> {
 				<img src={teamKickoffPicture}
 					 alt="The Raptors at their 2019 kickoff event."
 				     className="banner-image" />
-				<p>Robotics Team 1711, the RAPTORS, are a nationally competitive FIRST robotics team based in Traverse City, Michigan. The team prides themselves on their relentless commitment to community outreach, tireless work ethic and inspiring initiatives. The RAPTORS, fundamentally, are about more than just a robot; the team is built upon a unique atmosphere that goes beyond machining and coding. The RAPTORS are about encouraging innovation in all academic pursuits and fostering a culture that supports all learning endeavors.</p>
+				<p>
+					FIRST Robotics Team 1711, the RAPTORS, are a nationally competitive FIRST robotics team based in
+					Traverse City, Michigan. We pride ourselves on our relentless commitment to community outreach,
+					tireless work ethic and inspiring initiatives. The RAPTORS, fundamentally, are about more than just
+					robots; our team is built upon a unique atmosphere that goes beyond machining and programming. The
+					RAPTORS are about encouraging innovation in all academic pursuits and fostering a culture that
+					supports all learning endeavors.
+				</p>
 			</Page>
 		);
 		
