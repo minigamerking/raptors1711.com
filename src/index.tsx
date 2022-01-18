24598c2c27bf01ab0eaaebd602d67192a548a724
Trevor Sears <trevorsears.main@gmail.com>
2022-01-17 19:39:51 -0500

    Removed announcement banner regarding SSS rescheduling.

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,6 @@ import { ContactUs } from "./react/pages/contact-us";
 ReactDOM.render(
     <React.StrictMode>
         <BrowserRouter>
-            <AnnouncementBanner>Due to the rising COVID-19 numbers in our target age group (2-12 year olds) we have decided to postpone Super Science Saturday. This was a tough decision to make, but we feel for the safety of all involved that this is the best course of action. We are hoping to reschedule this event for the Spring of 2022.</AnnouncementBanner>
             <Routes>
                 <Route index element={<Navigate replace to="/home" />} />
                 <Route path="/index" element={<Navigate replace to="/home" />} />
