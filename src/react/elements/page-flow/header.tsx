9c53704f02277fd48d1450e6cddc6cb404117f1f
Trevor Sears <trevorsears.main@gmail.com>
2022-02-21 14:48:21 -0500

    Added submenus inside the header nav.

--- src/react/elements/page-flow/header.tsx
+++ src/react/elements/page-flow/header.tsx
@@ -35,7 +35,12 @@ export class Header extends React.Component<Props, State> {
 			"About Us": "/about-us",
 			"Outreach": "/outreach",
 			"Photo Gallery": "/photo-gallery",
-			"More": "/more"
+			"More": {
+				"More": "/contact-us",
+				"Meet the Leads": "/meet-the-leads",
+				"Meet the Mentors": "/meet-the-mentors",
+				"Sponsor Us": "/sponsorship"
+			}
 		};
 		
 	}
@@ -45,12 +50,48 @@ export class Header extends React.Component<Props, State> {
 		let navigationContent: React.ReactNode = Object.entries(this.props.links).map(
 			(entry: [string, string | { [name: string]: string }]): React.ReactNode => {
 				
-				let link: string = entry[1] as string;
-				let name: string = entry[0];
+				let navTitle: string = entry[0];
 				
-				return (
-					<Link to={link} key={name}>{name}</Link>
-				);
+				// For a plain link.
+				if (typeof entry[1] === "string") {
+					
+					let link: string = entry[1];
+					
+					return (
+						<div className="nav-item">
+							<Link className="nav-item-primary-link" to={link} key={navTitle}>{navTitle}</Link>
+						</div>
+					);
+					
+				// For a link with a submenu.
+				} else {
+					
+					let primaryLink: string | undefined = entry[1][navTitle];
+					
+					let dropdownContent: React.ReactNode = Object.entries(entry[1])
+						.filter((entry2: [string, string]): boolean => entry2[0] !== navTitle)
+						.map((entry2: [string, string]): React.ReactNode => {
+							
+							let entryTitle: string = entry2[0];
+							let entryLink: string = entry2[1];
+							
+							return <Link className="nav-item-dropdown-entry"
+										 to={entryLink} key={entryTitle}>{entryTitle}</Link>;
+							
+						}
+					);
+					
+					return (
+						<div className="nav-item">
+							<Link className="nav-item-primary-link" to={primaryLink} key={navTitle}>{navTitle}</Link>
+							<div className="nav-item-dropdown">
+								{dropdownContent}
+							</div>
+						</div>
+					);
+					
+				}
+				
 			}
 		);
 		
