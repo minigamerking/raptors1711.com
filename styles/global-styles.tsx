3ba64c46dbc02c0786c578f6a3ccb5bd49d5facd
Trevor Sears <trevor@trevorsears.com>
2022-10-18 15:50:05 -0400

    Updated a lot of global styles.

--- styles/global-styles.tsx
+++ styles/global-styles.tsx
@@ -11,6 +11,7 @@ import {
 import { Global, css, SerializedStyles } from "@emotion/react";
 import { generateColorDefinitionsCSSObject } from "@formativesolutions/colors";
 import { COLORS, RAW_COLORS } from "./colors";
+import { flexContainer } from "./mixins";
 
 const COLOR_VARIABLE_DEFINITIONS: Record<string, string> =
 	generateColorDefinitionsCSSObject(RAW_COLORS);
@@ -18,16 +19,30 @@ const COLOR_VARIABLE_DEFINITIONS: Record<string, string> =
 const styles: SerializedStyles = css({
 	":root": {
 		...COLOR_VARIABLE_DEFINITIONS,
-		backgroundColor: COLORS.WHITE,
-		color: COLORS.BLACK,
+		backgroundColor: COLORS.BLUEGRAY,
+		color: COLORS.WHITE,
+		fontSize: "16px",
 	},
 	"*": {
 		padding: 0,
 		margin: 0,
+		border: "none",
+		boxSizing: "border-box",
+		fontFamily: "'PT Sans', sans-serif",
+		lineHeight: "1.6",
 	},
 	"html, body": {
 		height: "100%",
 		width: "100%",
+	},
+	"#__next": {
+		...flexContainer({ mainAxis: "start" }),
+	},
+	"h1, h2, h3, h4, h5, h6": {
+		fontFamily: "'Spy Agency', sans-serif",
+	},
+	"a": {
+		color: "inherit",
 	}
 });
 
