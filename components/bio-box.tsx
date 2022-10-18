1f5c6ec9d7f4f55b5e7091d681acec575e917bed
Trevor Sears <trevor@trevorsears.com>
2022-10-18 15:52:21 -0400

    Added `<BioBox>` and `<BioListing>` for upcoming 'meet the team' pages.

---
+++ components/bio-box.tsx
@@ -0,0 +1,62 @@
+/*
+ * Created by Trevor Sears <trevor@trevorsears.com> (https://trevorsears.com/).
+ * 9:21 AM -- October 18th, 2022
+ * Project: v2.raptors1711.com
+ */
+
+import { ReactElement } from "react";
+import { Bio } from "../data/bio";
+import ContextBox from "./context-box";
+import { css, SerializedStyles } from "@emotion/react";
+
+export type Props = Readonly<{
+	bio: Bio,
+	imageSide?: "left" | "right",
+}>;
+
+const bioHeaderStyles: SerializedStyles = css({
+	display: "flex",
+	alignItems: "baseline",
+	justifyContent: "center",
+	flexWrap: "wrap",
+	marginBottom: "5px",
+	"*": {
+		padding: "0 5px",
+		whiteSpace: "nowrap",
+	},
+});
+
+const nameStyles: SerializedStyles = css({
+	fontSize: "1.6em",
+});
+
+const roleStyles: SerializedStyles = css({
+	fontSize: "1em",
+	opacity: "0.8",
+	fontWeight: "400",
+});
+
+const descriptionStyles: SerializedStyles = css({
+	textAlign: "center",
+});
+
+export default function BioBox({ bio, imageSide }: Props): ReactElement {
+	
+	return (
+		<ContextBox imageProportion={0.4}
+					imageSrc={bio.image}
+					imageAlt={bio.name}
+					imageSizes={undefined}
+					imageSide={imageSide}
+					imageVerticalCenter={bio.imageVerticalCenter}
+					imageHorizontalCenter={bio.imageHorizontalCenter}
+					centerChildren>
+			<div css={bioHeaderStyles}>
+				<h4 css={nameStyles}>{bio.name}</h4>
+				<h6 css={roleStyles}>{bio.role}</h6>
+			</div>
+			<p css={descriptionStyles}>{bio.description}</p>
+		</ContextBox>
+	);
+	
+}
---
+++ components/bio-listing.tsx
@@ -0,0 +1,50 @@
+/*
+ * Created by Trevor Sears <trevor@trevorsears.com> (https://trevorsears.com/).
+ * 9:10 AM -- October 18th, 2022
+ * Project: v2.raptors1711.com
+ */
+
+import { ReactElement, ReactNode } from "react";
+import { css, SerializedStyles } from "@emotion/react";
+import Image, { StaticImageData } from "next/image";
+import { PAGE_WIDTH } from "../styles/sizing";
+import { MENTORS } from "../data/mentors";
+import { Bio } from "../data/bio";
+import BioBox from "./bio-box";
+import { is } from "@babel/types";
+
+export type Props = Readonly<{
+	allBios: Record<string, Bio[]>,
+	year: number,
+	imageStartSide?: "left" | "right",
+}>;
+
+export default function BioListing({
+	allBios, year, imageStartSide
+}: Props): ReactElement | null {
+	
+	imageStartSide = imageStartSide ?? "left";
+	
+	const bios: Bio[] | undefined = allBios[year];
+	
+	if (bios === undefined) return null;
+	
+	const mentorBios: ReactNode = bios.map((bio: Bio, i: number): ReactNode => {
+		
+		let leftRightToggle: boolean = i % 2 !== 0;
+		
+		if (imageStartSide === "right") leftRightToggle = !leftRightToggle;
+		
+		const imageSide: "left" | "right" = leftRightToggle ? "left" : "right";
+		
+		return <BioBox bio={bio}
+				key={bio.name}
+				imageSide={imageSide} />
+		
+	});
+	
+	return (
+		<>{mentorBios}</>
+	);
+	
+}
