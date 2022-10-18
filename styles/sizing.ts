2b20406d1029837203f3fe7a09660982c60f117a
Trevor Sears <trevor@trevorsears.com>
2022-10-18 15:50:33 -0400

    Added 'sizing' variable container file.

---
+++ styles/sizing.ts
@@ -0,0 +1,14 @@
+/*
+ * Created by Trevor Sears <trevor@trevorsears.com> (https://trevorsears.com/).
+ * 12:05 PM -- October 17th, 2022
+ * Project: v2.raptors1711.com
+ */
+
+export const PAGE_WIDTH: string = "1000px";
+
+export const PAGE_MARGIN_TOP: string = "36px";
+export const PAGE_MARGIN_SIDES: string = "24px";
+export const PAGE_MARGIN_BOTTOM: string = "24px";
+
+export const PAGE_PADDING_HEADER: string = "36px";
+export const PAGE_PADDING_FOOTER: string = "36px";
