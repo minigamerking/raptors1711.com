f6176615ea0f385bc441033739f343e06836f571
Trevor Sears <trevor@trevorsears.com>
2022-10-21 09:54:52 -0400

    Added `<RaptorRobotics/>` component for logo text.

---
+++ components/raptor-robotics.tsx
@@ -0,0 +1,29 @@
+/*
+ * Created by Trevor Sears <trevor@trevorsears.com> (https://trevorsears.com/).
+ * 9:24 AM -- October 20th, 2022
+ * Project: v2.raptors1711.com
+ */
+
+import { ReactElement } from "react";
+import { css, SerializedStyles } from "@emotion/react";
+
+const textStyles: SerializedStyles = css({
+	fontSize: "2em",
+	lineHeight: "1",
+	textShadow: "2px 2px #0008",
+});
+
+const lineBreakStyles: SerializedStyles = css({
+	fontSize: 0,
+})
+
+export default function RaptorRobotics(): ReactElement {
+	
+	return (
+		<h1 css={textStyles}>
+			Raptor<br css={lineBreakStyles} />
+			Robotics
+		</h1>
+	);
+	
+}
